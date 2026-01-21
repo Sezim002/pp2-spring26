9f05116fb7dde1b645ac19204235be12fdcbd64e
Sezim <strymbek@mail.ru>
2026-01-21 19:49:41 +0500

    variables example

---
+++ practice1/variables.py
@@ -0,0 +1,35 @@
+#1 example 
+x = 13
+y = "PP2"
+print(x)
+print(y)
+
+
+#2 example 
+myvar = "Sezim"
+my_var = "Sezim"
+_my_var = "Sezim"
+myVar = "Sezim"
+MYVAR = "Sezim"
+myvar2 = "Sezim"  #Legal variable names
+
+#3 example 
+x, y, z = "KBTU", "SDU", "KIMEP"
+print(x)
+print(y)
+print(z)
+
+
+#4 example
+x = "YOU "
+y = "CAN  "
+z = "DO IT "
+print(x + y + z)
+
+
+#5 example 
+x = "beautiful girl"
+def myfunc():
+  print("SEZIM is " + x)
+
+myfunc()
