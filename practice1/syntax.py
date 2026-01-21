462a17fd401b3d9e4bb9de3fea951bf55f7c6bef
Sezim <strymbek@mail.ru>
2026-01-21 19:27:14 +0500

    syntax example

--- practice1/syntax.py
+++ practice1/syntax.py
@@ -1,6 +1,20 @@
+#1-example
 a = 5
 b = 7
 if a > b:
     print("a grater than b")
 else:
     print("b grater than a")
+
+#2-example
+x = 9
+y = "Hello, Sezim!"
+
+print(x)
+print(y)
+
+
+#3 example
+print("PP2 is easy!")
+print("Have a good experience.")
+print("Learning Python , do more!")
