f603e4fefa4c4c52aa38abb17c493c01729af44b
Sezim <strymbek@mail.ru>
2026-01-21 20:04:50 +0500

    numbers example

---
+++ practice1/numbers.py
@@ -0,0 +1,22 @@
+#1 example
+x = 9
+y = 124124124
+z = -6666666666
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# 2 example complex 
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#3 example 
+import random
+print(random.randrange(1, 10))
+ 
