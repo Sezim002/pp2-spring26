bb24c3f1c07e79e898917e500d18a24dd820d0af
Sezim <strymbek@mail.ru>
2026-01-21 20:10:00 +0500

    casting example

---
+++ practice1/casting.py
@@ -0,0 +1,14 @@
+# 1 example 
+x = int(1)   # x will be 1
+y = int(5.7) # y will be 5
+z = int("9") # z will be 9
+
+#2 example 
+x = float(1)     # x will be 1.0
+y = float(5.5)   # y will be 5.5
+z = float("5")   # z will be 5.0
+
+#3 example 
+x = str("s1") # x will be 's1'
+y = str(5)    # y will be '5'
+z = str(5.0)  # z will be '5.0'
