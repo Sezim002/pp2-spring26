ebe3f8871f0267df309208241c55dcda82410071
Sezim <strymbek@mail.ru>
2026-01-21 20:23:48 +0500

    string example

---
+++ practice1/string.py
@@ -0,0 +1,18 @@
+#1 example 
+print("first day")
+print("second day'")
+print('third day "')
+
+#2 example 
+for x in "cinema":
+  print(x)
+
+
+  #3 example 
+  #cheking 
+  txt = "your normal day someones dream!"
+print("dream" in txt)
+
+#4 example 
+a = "Hello, World!"
+print(len(a))
