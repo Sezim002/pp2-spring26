b498b4e15748eb75b261a3d9b1ad73c93222c016
Sezim <strymbek@mail.ru>
2026-01-27 10:18:31 +0500

    comparision operators example

---
+++ practice2/comparisonoperators.py
@@ -0,0 +1,34 @@
+#1 problem
+''' ==	Equal	x == y	
+!=	Not equal	x != y	
+>	Greater than	x > y	
+<	Less than	x < y	
+>=	Greater than or equal to	x >= y	
+<=	Less than or equal to	x <= y	'''
+#2 problem 
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+#3 problem 
+x = 5
+
+print(1 < x < 10)
+
+print(1 < x and x < 10)
+
+#4 problem 
+x=9
+y=10
+print(x>y)
+
+#5 problem 
+x = 9
+
+print(1 < x < 10)
+
