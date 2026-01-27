7c67d021cb49003cf7467c8a174b2888d4ac0ef0
Sezim <strymbek@mail.ru>
2026-01-27 10:28:23 +0500

    ifstatement example

---
+++ practice2/ifstatement.py
@@ -0,0 +1,26 @@
+#1 problem 
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+  #2 problem 
+  number = 15
+if number > 0:
+  print("The number is positive")
+  #3 problem 
+  age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+  #4problem 
+  is_logged_in = True
+if is_logged_in:
+  print("Welcome back!")
+  #5problem 
+  '''Equals: a == b
+Not Equals: a != b
+Less than: a < b
+Less than or equal to: a <= b
+Greater than: a > b
+Greater than or equal to: a >= b '''
