79a6bb110bb7a319bcc60fbc7968bcdd735c5c6d
Sezim <strymbek@mail.ru>
2026-01-30 14:11:26 +0500

    while continue

---
+++ practice2/wcontinue.py
@@ -0,0 +1,27 @@
+#1 problem 
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+  #2 problem 
+  i = 1
+while i <= 5:
+    print(i)
+    i = i + 1
+ #3 problem 
+ count = 1
+while count <= 3:
+    print("Hello")
+    count = count + 1
+#4 problem 
+num = 2
+while num < 10:
+    print(num)
+    num = num + 2
+#5 problem 
+answer = "no"
+
+while answer == "no":
+    answer = input("Do you want to continue? (yes/no): ")
