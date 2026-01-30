dadfb5a97da74c062ac284a3e7a23a29032f1585
Sezim <strymbek@mail.ru>
2026-01-30 14:16:42 +0500

    forloops example

---
+++ practice2/forloops.py
@@ -0,0 +1,24 @@
+#1 problem 
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  #2 problem 
+  total = 0
+
+for i in range(1, 6):
+    total = total + i
+
+print(total)
+#3 problem 
+word = "cat"
+
+for letter in word:
+    print(letter)
+#4 problem 
+for i in range(3):
+    print("Hello")
+#5 problem 
+numbers = [1, 2, 3, 4]
+
+for n in numbers:
+    print(n * 2)
