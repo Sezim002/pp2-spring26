d5971d56b85b10a296b7a6e7849987333e513caa
Sezim <strymbek@mail.ru>
2026-02-17 09:20:01 +0500

    math example

---
+++ practice4/math.py
@@ -0,0 +1,31 @@
+#1 problem 
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+#2 problem 
+x = abs(-777777)
+
+print(x)
+#3 problem 
+import math
+
+x = math.sqrt(64)
+
+print(x)
+#4 problem 
+import math
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) 
+print(y) 
+
+#5 problem 
+import math
+
+x = math.sqrt(64)
+
+print(x)
